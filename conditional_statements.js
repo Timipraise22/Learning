3b5110adcdbbffe30585d52864245ac60cb25199
Timipraise22 <timipraise22@gmail.com>
2025-05-26 11:05:38 -0600

    initial commit

---
+++ conditional_statements.js
@@ -0,0 +1,8 @@
+//if else statement
+var activeStatus = false;
+if(activeStatus = true ){
+    console.log("Send a message to Chief telling him about the status of the work")
+}
+else{
+    console.log("Call him and speak to him about it over the phone ")
+}
