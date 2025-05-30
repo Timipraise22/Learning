837c006b2d23416d971364bdc349ea4cce529bcc
Timipraise22 <timipraise22@gmail.com>
2025-05-30 01:27:36 -0600

    added new lines

--- Loops.js
+++ Loops.js
@@ -1,1 +1,49 @@
-console.log("hat")
+// //for loop
+// for(var i = 1; i<= 10; i++){
+//     console.log(i)
+// }
+
+// //using conditional satements with loop
+// for(var i = 1; i<= 10; i++){
+//     if (i % 2 ==1){        // % represents remainder
+//     console.log(i)
+//     }
+// }
+
+// // using break statements 
+// for(var i = 1; i<= 10; i++){
+//     if(i > 5){
+//         break;
+//     }
+//     console.log(i)
+// }
+
+// WHile loops
+// var i = 1;
+// while(i<10){
+// console.log(i++)
+// }
+
+//Another example
+// var age = 7;
+// while (age<11){
+//     console.log("you are less thanthe age requirements");
+//     age++;
+// }
+// console.log("you have passed the age requirements")
+
+// Do while Loop
+var i = 1;
+do{
+    console.log(i);
+    i++;
+}
+while(i<7);
+
+//For Each Loop
+var age = 10;
+do{
+    console.log(age)
+    age++
+}
+while(age< 18);
