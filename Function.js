fc50f5ecc2329048a8e7862d63dcde7fa63b5ebe
Timipraise22 <timipraise22@gmail.com>
2025-06-10 21:36:44 +0100

    commited

--- Function.js
+++ Function.js
@@ -3,9 +3,27 @@ function age(){
 }
 age();
 
-// ways of calling function when you want to add another dtat types
+// ways of calling function when you want to add another data types
 function name(firstname){
     console.log(`my name is ${firstname} and i do web programming`)
 }
 
 name("yamal")
+
+//getting values with functions 
+function getAverage(a,b){
+    var average = (a + b) / 5;
+    console.log(average);
+}
+
+getAverage(10,25);
+
+//using functions to return value from the previous example 
+function getAverage(a,b,c,d,e){
+    var average = (a + b + c + d + e) / 5;
+    console.log(average);
+    return average;
+}
+
+var grade = getAverage(10,20,18,16,9);
+console.log(`Your grade is ${grade}`)
